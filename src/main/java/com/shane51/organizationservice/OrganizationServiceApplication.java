795594251b25c97f8dc1cbd0dfa877ee3f9e6364
Wuyi Qin <wyqin@thoughtworks.com>
2018-10-26 11:13:48 +0800

    [Tech] Enable swagger docs

--- src/main/java/com/shane51/organizationservice/OrganizationServiceApplication.java
+++ src/main/java/com/shane51/organizationservice/OrganizationServiceApplication.java
@@ -7,16 +7,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableSwagger2
 public class OrganizationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrganizationServiceApplication.class, args);
     }
 
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shane51.organizationservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization V1.0").build());
+    }
 
     @Bean
     OrganizationRepository repository() {
